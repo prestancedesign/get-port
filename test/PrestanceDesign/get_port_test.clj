695b004bd018269a28a1990b8db16a39d80a795f
Michael SALIHI <admin@prestance-informatique.fr>
2020-12-10 21:46:32 +0100

    Adding first tests

--- test/PrestanceDesign/get_port_test.clj
+++ test/PrestanceDesign/get_port_test.clj
@@ -1,7 +1,12 @@
 (ns PrestanceDesign.get-port-test
   (:require [clojure.test :refer :all]
-            [PrestanceDesign.get-port :refer :all]))
+            [PrestanceDesign.get-port :refer :all])
+  (:import (java.net ServerSocket BindException)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest get-available-port-test
+  (testing "Test binding port 3000"
+    (is (= 3000 (get-available-port 3000)))))
+
+(deftest get-port-in-range-test
+  (testing "Test bind port from a range"
+    (is (= 3000 (get-port-in-range {:from 3000 :to 3005})))))
