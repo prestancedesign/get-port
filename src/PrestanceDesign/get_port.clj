2d1a87584614c2e55e1c06f3a5cc7cc09a3ee30d
Michael SALIHI <admin@prestance-informatique.fr>
2020-12-10 21:43:55 +0100

    Function to get a random or specific port and a port between a range

--- src/PrestanceDesign/get_port.clj
+++ src/PrestanceDesign/get_port.clj
@@ -1,10 +1,31 @@
-(ns PrestanceDesign.get-port)
+(ns PrestanceDesign.get-port
+  (:import (java.net ServerSocket BindException)))
 
-(defn get-available-port []
-  (let [socket (java.net.ServerSocket. 0)]
+(def range-port {:from 3000 :to 3005})
+
+(defn get-available-port [& [port]]
+  (let [socket (ServerSocket. (or port 0))]
     (.close socket)
     (.getLocalPort socket)))
 
-(comment
-  (get-available-port))
+(defn get-port-in-range [range]
+  (loop [current-port (:from range)]
+    (when (<= current-port (:to range))
+      (let [result
+            (try
+              (get-available-port current-port)
+              (catch Exception e (instance? BindException (.getCause e))))]
+        (or result (recur (inc current-port)))))))
 
+(comment
+
+  (get-port-in-range range-port)
+
+  (get-available-port 3001)
+  (get-available-port)
+
+  ;; Bind a range of port for testing
+  (for [port (range 3000 3005)]
+    (try
+      (ServerSocket. port)
+      (catch BindException e (str "caught exception: " (.getMessage e))))))
