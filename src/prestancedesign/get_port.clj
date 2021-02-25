568aedb26886f0bb956464ae289dbbf9696a13d5
Michael SALIHI <admin@prestance-informatique.fr>
2021-02-25 12:19:52 +0100

    Adding docstrings

--- src/prestancedesign/get_port.clj
+++ src/prestancedesign/get_port.clj
@@ -1,14 +1,27 @@
 (ns prestancedesign.get-port
   (:import (java.net ServerSocket BindException)))
 
-(defn- get-available-port [& [port]]
+(defn- get-available-port
+  "Return a random available TCP port in allowed range (between 1024 and 65535) or a specified one"
+  [& [port]]
   (with-open [socket (ServerSocket. (or port 0))]
     (.getLocalPort socket)))
 
-(defn make-range [from to]
+(defn make-range
+  "Make a range of ports.
+  Must be in the range 1024...65535"
+  [from to]
   (range from (inc to)))
 
-(defn get-port [& [options]]
+(defn get-port
+  "Get an available TCP port according to the supplied options.
+
+  - A preferred port: (get-port {:port 3000})
+  - A vector of preferred ports: (get-port {:port [3000 3004 3010]})
+  - Use the `make-range` helper in case you need a port in a certain (inclusive) range: (get-port {:port (make-range 3000 3005)})
+
+  No args return a random available port"
+  [& [options]]
   (if-let [ports (:port options)]
     (loop [port ports]
       (let [result
